97e6582559d228fc506ec988d9f7e3525d8b8739
IoannaMi <ioannamicha@hotmail.com>
2020-12-15 15:18:10 +0100

    add exception messages at wps_chw

--- processes/wps_chw2.py
+++ processes/wps_chw2.py
@@ -87,39 +87,44 @@ class WpsChw20(Process):
 
     def _handler(self, request, response):
         """Handler function of the WpsChw2"""
-        # try:
-        print("request was made")
-        # Read input
-        line_str = request.inputs["transect"][0].data
-        # load geojson
-        line_geojson = geojson.loads(line_str)
-        # Initiate chw object
-        chw = CHW(line_geojson)
-        # 1st level check
-        chw.get_info_geological_layout()
-        # 2nd level check
-        chw.get_info_wave_exposure()
-        # 3rd level check
-        chw.get_info_tida_range()
-        # 4th level check
-        chw.get_info_flora_fauna()
-        # 5th level check
-        chw.get_info_sediment_balance()
-        # 6th level check
-        chw.get_info_storm_climate()
 
-        # classify hazards according to coastalhazardwheel decision tree
-        chw.hazards_classification()
-        # get proposed measures
-        chw.provide_measures()
+        try:
+            # Read input
+            line_str = request.inputs["transect"][0].data
+            # load geojson
+            line_geojson = geojson.loads(line_str)
+        except Exception:
+            msg = "Failed to read the input"
+            res = {"errMsg": msg}
+            response.outputs["output_json"].data = json.dumps(res)
+        try:
+            # Initiate chw object
+            chw = CHW(line_geojson)
+            # 1st level check
+            chw.get_info_geological_layout()
+            # 2nd level check
+            chw.get_info_wave_exposure()
+            # 3rd level check
+            chw.get_info_tida_range()
+            # 4th level check
+            chw.get_info_flora_fauna()
+            # 5th level check
+            chw.get_info_sediment_balance()
+            # 6th level check
+            chw.get_info_storm_climate()
+        except Exception:
+            msg = "Something went wrong in the retrieval of the information"
+            res = {"errMsg": msg}
+            response.outputs["output_json"].data = json.dumps(res)
 
-        chw.db.close_db_connection()
-
-        output = write_output(chw)
-        response.outputs["output_json"].data = json.dumps(output)
-
-        # close_db_connection()
-        # except Exception as e:
-        # res = {"errMsg": "ERROR: {}".format(e)}
-        # response.outputs["output_json"].data = json.dumps(res)
-        # return response
+        try:
+            # classify hazards according to coastalhazardwheel decision tree
+            chw.hazards_classification()
+            # get measures
+            chw.provide_measures()
+            output = write_output(chw)
+            response.outputs["output_json"].data = json.dumps(output)
+        except Exception:
+            msg = "Something went wrong during the classification"
+            res = {"errMsg": msg}
+            response.outputs["output_json"].data = json.dumps(res)
