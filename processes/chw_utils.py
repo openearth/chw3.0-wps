9279fdc6ff7c2f800fd053d93bb9102c2e5df661
IoannaMi <ioannamicha@hotmail.com>
2021-01-22 18:55:24 +0100

    change priority; globalcover for pattern of barriers, transect20km no point on coast

--- processes/chw_utils.py
+++ processes/chw_utils.py
@@ -82,16 +82,17 @@ class CHW:
 
         self.transect_length = change_coords(self.transect).length
 
-        # in wkt format as returned from database
+        # 10km from the point on the coast and -180 direction
         self.transect10km = self.db.ST_line_extend(
             wkt=self.transect_wkt, P=self.point_on_coast, dist=10000, direction=-180
         )
+        # 100km from the point on the coast and -180 deg direction
         self.transect100km = self.db.ST_line_extend(
             wkt=self.transect_wkt, P=self.point_on_coast, dist=100000, direction=-180
         )
-
+        # 20 km transect = + 20km from the sea point of the given transect (180 deg direction)
         self.transect20km = self.db.ST_line_extend(
-            wkt=self.transect_wkt, P=self.point_on_coast, dist=20000, direction=180
+            wkt=self.transect_wkt, dist=20000, direction=180
         )
 
         self.bbox_20km = get_bounds(self.transect20km)
@@ -99,7 +100,9 @@ class CHW:
         # get dem
 
         cut_wcs(*self.bbox_20km, dem_layer, owsurl, self.dem)
-
+        # TODO pass the path only and or give another name to the oufname.
+        # it is not an output of elevation profile but something that I use
+        # in the whole process
         self.elevations, self.segments = get_elevation_profile(
             dem=self.dem,
             line=change_coords(self.transect20km),
@@ -109,19 +112,26 @@ class CHW:
 
         self.slope = round(calc_slope(self.elevations, self.segments), 3)
 
+        self.geology = self.db.get_geol_glim_values(self.transect_wkt)
+
     # 1st level check
     def get_info_geological_layout(self):
-        if self.check_barrier():
-            self.geological_layout = "Barrier"
 
-        elif self.db.intersect_with_estuaries(self.transect_wkt) and self.slope < 3:
+        if self.db.intersect_with_estuaries(self.transect_wkt) and self.slope < 3:
             self.geological_layout = "Delta/ low estuary island"
 
-        elif self.db.intersect_with_corals(self.transect_wkt):
-            self.geological_layout = "Coral island"
+        elif self.geology != [] and self.check_barrier() is True:
+            self.geological_layout = "Barrier"
 
-        else:
+        elif self.geology != []:
             self.geological_layout = self.check_geology_type()
+        # TODO add also small islands? if yes then perhaps consider to move all this to a
+        # function
+        elif (
+            self.geology == []
+            and self.db.intersect_with_corals(self.transect_wkt) is True
+        ):
+            self.geological_layout = "Coral island"
 
     # 2nd level check
     def get_info_wave_exposure(self):
@@ -133,17 +143,21 @@ class CHW:
         If moderately exposed it can drop to protected(<10 km closest coastline that protects it)
         """
         self.wave_exposure = self.db.get_wave_exposure_value(self.transect_wkt)
+        print("self.wave_exposure", self.wave_exposure)
 
         if self.wave_exposure == "moderately exposed":
             closest_coasts = self.db.fetch_closest_coasts(self.transect10km)
-            if len(closest_coasts) > 0:
+            print("closest_coasts 10km", closest_coasts)
+            if len(closest_coasts) > 1:
                 self.wave_exposure = "Protected"
         elif self.wave_exposure == "exposed":
             closest_coasts = self.db.fetch_closest_coasts(self.transect100km)
-            if len(closest_coasts) > 0:
+            print("closest_coasts 100km", closest_coasts)
+            if len(closest_coasts) > 1:
                 self.wave_exposure = "moderately exposed"
             closest_coasts = self.db.fetch_closest_coasts(self.transect10km)
-            if len(closest_coasts) > 0:
+            print("closest_coasts 10km", closest_coasts)
+            if len(closest_coasts) > 1:
                 self.wave_exposure = "Protected"
 
     # 3rd level check
@@ -285,28 +299,23 @@ class CHW:
             str: The name of the geology type
         """
 
-        geology_values = self.db.get_geol_glim_values(self.transect_wkt)
-        # TODO change the name su_values to something more general. Before the code
-        # had only su values in the check
-        su_values = sum(
-            x in [("su",), ("sm",), ("ss",), ("sc",)] for x in geology_values
-        )
-        non_su_values = sum(
-            x not in [("su",), ("sm",), ("ss",), ("sc",)] for x in geology_values
+        unconsol = sum(x in [("su",), ("sm",), ("ss",), ("sc",)] for x in self.geology)
+        non_unconsol = sum(
+            x not in [("su",), ("sm",), ("ss",), ("sc",)] for x in self.geology
         )
 
-        if su_values >= non_su_values:
-            geology = "su"
+        if unconsol >= non_unconsol:
+            geology_type = "soft"
         else:
-            geology = ""
+            geology_type = "hard"
 
-        if geology == "su" and self.slope <= 3:
+        if geology_type == "soft" and self.slope <= 3:
             return "Sediment plain"
 
-        elif geology == "su" and self.slope > 3:
+        elif geology_type == "soft" and self.slope > 3:
             return "Sloping soft rock"
 
-        elif geology != "su" and self.slope <= 3:
+        elif geology_type == "hard" and self.slope <= 3:
             return "Flat hard rock"
 
         else:
@@ -321,16 +330,30 @@ class CHW:
             The pattern that is used here detects no-data - data from the elevation dataset(MERIT-Coast) over a transect of 20 km.
             If this pattern is detected then it is classified as barrier.
         """
-        sea_pattern = detect_sea_patterns(self.elevations)
+        globcover20km = Path(self.tmp) / "globcover_20km.tif"
+        cut_wcs(*self.bbox_20km, landuse_layer, owsurl, globcover20km)
+        # Detect sea pattern: Sea, land, sea, land
+        sea_pattern = detect_sea_patterns(globcover20km)
+        print("sea_pattern", sea_pattern)
         land_sea_changes = np.argwhere(sea_pattern == True)
+        print("land_sea_changes", land_sea_changes)
 
-        if land_sea_changes.shape[0] > 1:
+        # Check if unconsolitated values on the coast
+        unconsol = sum(x == ("su",) for x in self.geology)
+        non_unconsol = sum(x != ("su",) for x in self.geology)
+        print("Detect if barrier", unconsol, non_unconsol)
+
+        if unconsol >= non_unconsol and land_sea_changes.shape[0] > 1:
             barrier = True
         else:
             barrier = False
         return barrier
 
+    # TODO move function at raster_utils
     def get_vegetation(self):
+        # TODO I need globcover for barrier land no land patter.
+        # for now cut twice (as I need two different bboxes) cant estimate vegetation
+        # with a bbox of 20km
         cut_wcs(*self.bbox, landuse_layer, owsurl, self.globcover)
         values = read_raster_values(self.globcover)
         non_vegetated = np.count_nonzero(np.logical_and(values >= 190, values <= 220))
