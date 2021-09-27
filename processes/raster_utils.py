ac1916e4254d60b8ce49bc0e1fadac240cda05e4
IoannaMi <ioannamicha@hotmail.com>
2021-09-27 15:52:28 +0200

    gdalwarp on cmd

--- processes/raster_utils.py
+++ processes/raster_utils.py
@@ -30,12 +30,13 @@ from .wcs_utils import LS
 import logging
 import numpy as np
 
-from osgeo import gdal
+
 import rasterio
 from rasterio.warp import calculate_default_transform, reproject, Resampling
 from scipy.stats import linregress
 from statistics import mean, median
 from pathlib import Path
+import os
 
 
 def cut_wcs(
@@ -63,17 +64,8 @@ def cut_wcs(
 
 
 def reproject_raster(infname, outfname):
-    in_raster = gdal.Open(infname)
-    driver = "GTiff"
-    memory = 2e3
-    new_projection = """PROJCS["WGS 84 / Pseudo-Mercator",GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]],PROJECTION["Mercator_1SP"],PARAMETER["central_meridian",0],PARAMETER["scale_factor",1],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["X",EAST],AXIS["Y",NORTH],EXTENSION["PROJ4","+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs"],AUTHORITY["EPSG","3857"]]"""
-    gdal.Warp(
-        outfname,
-        in_raster,
-        dstSRS=new_projection,
-        warpMemoryLimit=memory,
-        format=driver,
-    )
+    cmd = f"""gdalwarp -s_srs EPSG:4326 -t_srs EPSG:3857 {infname} {outfname}"""
+    os.system(cmd)
 
 
 def reproject_raster_rasterio(infname, outfname, dst_crs="EPSG:3857"):
